a83cfc112ed262c6294e5341a0d41ac3464c1271
AmirHossein Shakeri <amirhossein.shk@gmail.com>
2019-12-24 15:39:17 +0330

    some changes

--- src/components/form-wizard/PropertyDetails.vue
+++ src/components/form-wizard/PropertyDetails.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    Property Details Goes Here!
+  <div class="property-details">
+    <div class="mt-4">
+      <div class="row">
+        <div class="col-md-12">
+          <h4 class="font-weight-bold">Property details</h4>
+          <p class="mt-5">Are you first time homebuyer?</p>
+        </div>
+        <div class="col-md-12">
+          <button class="mr-3">Yes</button>
+          <button class="no-button">No</button>
+        </div>
+        <div class="col-md-12 mt-4">
+          <p>When do you plan to purchase your home?</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
